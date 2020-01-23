f59201fa0776eb772f617390f289c8b9221d25c9
Simon Laux <mobile.info@simonlaux.de>
2020-01-23 03:14:34 +0100

    format setup.ts

--- src/main/setup.ts
+++ src/main/setup.ts
@@ -3,19 +3,19 @@ import { getLogger } from '../shared/logger'
 /**
  * Only works when it's installed (aka when its a dev enviroment)
  */
-export async function tryInstallReactDevTools () {
-  const log = getLogger('main/dev')
-  try {
-    const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer')
+export async function tryInstallReactDevTools() {
+    const log = getLogger('main/dev')
     try {
-      const name = await installExtension(REACT_DEVELOPER_TOOLS)
-      log.debug(`Added Extension:  ${name}`)
-    } catch (err) {
-      log.debug('An error occurred: ', err)
+        const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer')
+        try {
+            const name = await installExtension(REACT_DEVELOPER_TOOLS)
+            log.debug(`Added Extension:  ${name}`)
+        } catch (err) {
+            log.debug('An error occurred: ', err)
+        }
+    } catch (error) {
+        log.debug('react devtools aren\'t loaded (this is normal when using a production version of dc)')
     }
-  } catch (error) {
-    log.debug('react devtools aren\'t loaded (this is normal when using a production version of dc)')
-  }
 }
 
 
